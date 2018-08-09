6efb72070ac24050175e598dd44868c78924be4d
Richard Bornat <richard@bornat.me.uk>
2018-08-09 15:40:23 +0100

    with some more help for pairs and triples

--- Tupleutils.ml
+++ Tupleutils.ml
@@ -38,3 +38,23 @@ let string_of_triple string_of_a string_of_b string_of_c sep (a,b,c) =
                           
 let bracketed_string_of_triple string_of_a string_of_b string_of_c triple = 
     "(" ^ string_of_triple string_of_a string_of_b string_of_c "," triple ^ ")"
+
+let two_map     f   (a,b) = (f a, f b)
+let two_fold    f x (a,b) = List.fold_left f x [a;b]
+let two_mapfold f x (a,b) = 
+  let x, a = f x a in
+  let x, b = f x b in
+  x, (a,b)
+
+let fstof3  (x,_,_) = x
+let sndof3  (_,y,_) = y
+let thrdof3 (_,_,z) = z
+
+let three_map     f   (a,b,c) = (f a, f b, f c)
+let three_fold    f x (a,b,c) = List.fold_left f x [a;b;c]
+let three_mapfold f x (a,b,c) = 
+  let x, a = f x a in
+  let x, b = f x b in
+  let x, c = f x c in
+  x, (a,b,c)
+

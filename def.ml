ef1675e70c470a28ba161bc625f480b9c7748d64
Richard Bornat <richard@bornat.me.uk>
2018-08-15 09:57:13 +0100

    prints fun and proc in defs

--- def.ml
+++ def.ml
@@ -32,11 +32,11 @@ type def =
   | Functiondef of name instance * pattern list * expr 
 
 let rec string_of_def = function
-  | Processdef  (pn,params,proc) -> Printf.sprintf "%s(%s) = %s"
+  | Processdef  (pn,params,proc) -> Printf.sprintf "proc %s(%s) = %s"
                                        (string_of_name pn.inst)
                                        (String.concat "," (List.map string_of_param params))
                                        (string_of_process proc)
-  | Functiondef (fn,params,expr) -> Printf.sprintf "%s %s = %s"
+  | Functiondef (fn,params,expr) -> Printf.sprintf "fun %s %s = %s"
                                        (string_of_name fn.inst)
                                        (String.concat " " (List.map string_of_fparam params))
                                        (string_of_expr expr)

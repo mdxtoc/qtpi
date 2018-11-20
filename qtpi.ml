1cf8dbf9d328147a5588da8780ae86d79f5d4b84
Richard Bornat <richard@bornat.me.uk>
2018-11-20 12:04:21 +0000

    better exception printing

--- qtpi.ml
+++ qtpi.ml
@@ -76,6 +76,7 @@ let _ = match !Usage.files with
                                                            (string_of_sourcepos outer.pos)
                 | Parseutils.Error s     -> print_endline s
                 | Library.Abandon s      -> Printf.printf "\n\n** %s -- execution abandoned\n" s
+                | Settings.Can'tHappen s -> Printf.printf "!! Can't Happen !! -- %s" s
                 | exn                    -> Printf.printf "\n\n** unexpected exception %s **\n"
                                                           (Printexc.to_string exn)
 

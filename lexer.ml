c90ebe926503ae39338251e6eb08c84ed3bbfa2f
Richard Bornat <richard@bornat.me.uk>
2020-03-20 14:14:36 +0000

    This is actually a rename and a massive rewrite to convert to Sedlex, so I can include unicode characters.

---
+++ lexer.ml
@@ -0,0 +1,213 @@
+(*
+    Copyright (C) 2018 Richard Bornat
+     
+        richard@bornat.me.uk
+
+    This file is part of Qtpi, an interpreter for Gay and Nagarajan's CQP.
+
+    Qtpi is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Qtpi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Qtpi in the file LICENSE; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    (or look at http://www.gnu.org).
+*)
+
+
+open Parser
+open Functionutils
+
+exception LexError of Sourcepos.sourcepos * string
+  
+let inc_linenum lexbuf =
+  Sedlexing.new_line lexbuf 
+
+let get_linenum lexbuf = 
+  let (_,start_p) = Sedlexing.lexing_positions lexbuf in
+  start_p.Lexing.pos_lnum
+  
+let get_loc lexbuf : Lexing.position * Lexing.position =
+  Sedlexing.lexing_positions lexbuf
+
+(* this thing is ok, because we read utf8 chars byte by byte *)
+let stringbuffer = Buffer.create 256
+
+let bkconvert lexeme = Array.sub lexeme 1 (Array.length lexeme - 2)
+
+let string_of_lexeme = String.concat "" <.> List.map Utf8.string_of_uchar <.> Array.to_list
+
+let _BLANK = [%sedlex.regexp? Chars " \t" ]
+let _NEWLINE = [%sedlex.regexp? '\n' | "\r\n" ]
+
+let _DIGIT = [%sedlex.regexp? '0'..'9' ]
+let _ALPHA =  [%sedlex.regexp? 'a'..'z' | 'A'..'Z' ]
+let _ALPHANUM = [%sedlex.regexp? _ALPHA | _DIGIT | '_' ]
+
+let number = [%sedlex.regexp? Plus _DIGIT, Opt ('.', Plus _DIGIT), Opt (Chars"eE", Opt('-'), Plus _DIGIT) ]
+
+let name   = [%sedlex.regexp? _ALPHA, Star (_ALPHA | _DIGIT | '_' | '\'') ]
+
+let tvname = [%sedlex.regexp? '\'', Opt(Chars "\'^*"), name ]
+
+let tpnum = [%sedlex.regexp? Plus _DIGIT, Star ('.', Plus _DIGIT) ]
+
+let bke = [%sedlex.regexp? Chars "01+-" ]
+
+let bra = [%sedlex.regexp? '<', Plus bke, '|' ]
+let ket = [%sedlex.regexp? '|', Plus bke, '>' ]
+
+let rec make_token : Sedlexing.lexbuf -> Parser.token = fun lexbuf ->
+  match%sedlex lexbuf with
+  | _BLANK      -> make_token lexbuf (* Skip blanks and tabs *)
+  | _NEWLINE    -> inc_linenum lexbuf; make_token lexbuf
+  | "(*"        -> bracomment (get_loc lexbuf) lexbuf; make_token lexbuf 
+
+  | eof         -> EOP   (* Give up on end of file *)
+
+  | '('         -> LPAR
+  | ')'         -> RPAR
+  | '['         -> LSQPAR
+  | ']'         -> RSQPAR
+  | '|'         -> PARSEP
+  | ':'         -> COLON
+  | '='         -> EQUALS
+  | '*'         -> STAR
+  | "><"        -> TENSORPROD
+  | "><><"      -> TENSORPOWER
+  | "<-"        -> LEFTARROW
+  
+  | "true"      -> TRUE
+  | "false"     -> FALSE
+  
+  | "if"        -> IF
+  | "then"      -> THEN
+  | "else"      -> ELSE
+  | "elif"      -> ELIF
+  | "elsf"      -> ELIF
+  | "fi"        -> FI
+    
+  | "num"       -> NUMTYPE
+  | "bool"      -> BOOLTYPE
+  | "bit"       -> BITTYPE
+  | "gate"      -> GATETYPE
+  | "qbit"      -> QBITTYPE
+  | "qstate"    -> QSTATETYPE
+  | "^"         -> CHANTYPE
+  | "char"      -> CHARTYPE
+  | "string"    -> STRINGTYPE
+  | "matrix"    -> MATRIXTYPE
+  | "bra"       -> BRATYPE
+  | "ket"       -> KETTYPE
+  
+  | '.'         -> DOT
+  | ".."        -> DOTDOT
+    
+  | "new"       -> NEWDEC
+  | "untraced"  -> UNTRACED
+  | "newq"      -> QBITDEC
+  | "let"       -> LETDEC
+  | "match"     -> MATCH
+  
+  | "fun"       -> FUN
+  | "proc"      -> PROC
+  | "with"      -> WITH
+  | "where"     -> WHERE
+  | "lam"       -> LAMBDA
+  | "/^"        -> TESTPOINT
+  | ".*"        -> PROCITER
+  
+  | '?'         -> QUERY
+  | '!'         -> BANG
+  | "-/-"       -> MEASURE
+  | ">>"        -> THROUGH
+  
+  | ','         -> COMMA
+  | ';'         -> SEMICOLON
+  
+  | '+'         -> PLUS
+  | '-'         -> MINUS
+  | '/'         -> DIV
+  | '%'         -> MOD
+  (* and STAR as multiply *)
+  | "**"        -> POW
+    
+  | '='         -> EQUALS
+  | "<>"        -> NOTEQUAL
+  | '<'         -> LESS
+  | "<="        -> LESSEQUAL
+  | ">="        -> GREATEREQUAL
+  | '>'         -> GREATER
+  
+  | "&&"        -> AND
+  | "||"        -> OR
+  | "not"       -> NOT
+  
+  | '@'         -> APPEND
+  | "::"        -> CONS
+      
+  | '_'         -> UNDERSCORE
+  
+  | "_0"        -> TERMINATE
+  
+  | "0b0"       -> BIT0
+  | "0b1"       -> BIT1
+  
+  | "All"       -> FORALL
+  | "process"   -> PROCESS
+  
+  | "->"        -> TYPEARROW
+    
+  | "'", Compl (Chars "'\\\n\r\t"), "'"
+                -> CHAR (Sedlexing.lexeme_char lexbuf 1)
+  | "'\\", Chars "\\'\"ntbr ", "'"
+                ->  CHAR(Utf8.unescaped (Sedlexing.lexeme_char lexbuf 2)) 
+  | "'\\", any  ->  raise (LexError(get_loc lexbuf, Printf.sprintf "illegal escape \\%s" (Utf8.string_of_uchar (Sedlexing.lexeme_char lexbuf 2))))
+  | '"'         ->  Buffer.clear stringbuffer;
+                    STRING (string (get_loc lexbuf) lexbuf)
+                
+
+  | number      -> NUM (string_of_lexeme (Sedlexing.lexeme lexbuf))
+  | name        -> NAME (string_of_lexeme (Sedlexing.lexeme lexbuf))   (* should be interned *)
+  | tvname      -> TVNAME (string_of_lexeme (Sedlexing.lexeme lexbuf)) (* should be interned *)
+  | tpnum       -> TPNUM (string_of_lexeme (Sedlexing.lexeme lexbuf))
+  
+  | bra         -> BRA (string_of_lexeme (bkconvert (Sedlexing.lexeme lexbuf)))
+  | ket         -> KET (string_of_lexeme (bkconvert (Sedlexing.lexeme lexbuf)))
+
+  | any         -> raise (LexError (get_loc lexbuf, Printf.sprintf "Invalid character '%s'"
+                                                                (Utf8.escaped (Sedlexing.lexeme_char lexbuf 0))
+                                   )
+                         )
+  | _           -> raise (Settings.Can'tHappen "bottom of Lexer.make_token")
+  
+and bracomment spos lexbuf = 
+    match%sedlex lexbuf with
+    |   "(*"        ->  bracomment (get_loc lexbuf) lexbuf; 
+                        bracomment spos lexbuf
+    |   "*)"        ->  () 
+    |   _NEWLINE    ->  inc_linenum lexbuf; bracomment spos lexbuf 
+    |   eof         ->  raise (LexError (spos, "unmatched comment-bracket '(*'")) 
+    |   any         ->  bracomment spos lexbuf 
+    | _             -> raise (Settings.Can'tHappen "bottom of Lexer.bracomment")
+  
+      
+and string spos lexbuf = 
+    match%sedlex lexbuf with
+    | '"'                       ->  Buffer.contents stringbuffer 
+    | '\\', Chars "\\'\"ntbr "  ->  let c = Utf8.unescaped (Sedlexing.lexeme_char lexbuf 1) in
+                                    Buffer.add_utf_8_uchar stringbuffer c; string spos lexbuf 
+                                                
+    | eof                       ->  raise (LexError (spos, "unterminated string")) 
+    | any                       ->  Buffer.add_utf_8_uchar stringbuffer (Sedlexing.lexeme_char lexbuf 0); string spos lexbuf 
+    | _                         -> raise (Settings.Can'tHappen "bottom of Lexer.string")
+ 
+(* let build_prog_from_string s =
+    Parser.program make_token (Lexing.from_string s) *)

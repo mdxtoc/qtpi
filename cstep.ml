8a86102f08a680364674b30981ccf52645da3e3d
Richard Bornat <richard@bornat.me.uk>
2020-05-11 18:58:25 +0100

    using rtenv

--- cstep.ml
+++ cstep.ml
@@ -26,32 +26,34 @@ open Stringutils
 open Optionutils
 open Instance
 open Name
+open Vt
 open Cbasics
 open Type
 open Param
 
 type ciostep = ciostumble instance
 
-and ciostumble =
-  | CRead of cexpr * _type * env cpattern
-  | CWrite of cexpr * _type * cexpr            (* argument cexpr can, of course, be a tuple *)
+and ciostumble = (* type needed for tracing. Sorry *)
+  | CRead of cexpr * _type * rtenv cpattern
+  | CWrite of cexpr * _type * cexpr                                     (* argument cexpr can, of course, be a tuple *)
   
 type cqstep = cqstumble instance
 
 and cqstumble =
-  | CMeasure of bool * cexpr * cexpr option * env cpattern  (* plural, qbit, basis gate, cpattern *)
-  | CThrough of bool * cexpr list * cexpr                   (* plural, qbits, gate *)
+  | CMeasure of bool * cexpr * cexpr option * rtenv cpattern    (* plural, qbit, basis gate, cpattern *)
+  | CThrough of bool * cexpr list * cexpr                       (* plural, qbits, gate *)
   
 let string_of_ciostep ciostep =
   match ciostep.inst with
-  | CRead (ce,t,pat)       -> Printf.sprintf "%s?(%s:%s)"
+  | CRead (ce,tpat,pat)         -> Printf.sprintf "%s?(%s:%s)"
                                           (string_of_cexpr ce)
                                           (string_of_cpattern pat)
-                                          (string_of_type t)
-  | CWrite (ce,t,e)        -> Printf.sprintf "%s!(%s:%s)"
+                                          (string_of_type tpat)
+  | CWrite (ce,te,e)            -> Printf.sprintf "%s!(%s:%s)"
                                           (string_of_cexpr ce)
                                           (string_of_cexpr e)
-                                          (string_of_type t)
+                                          (string_of_type te)
+
 let string_of_cqstep cqstep =
   match cqstep.inst with
   | CMeasure (plural,e,gopt,p)   -> Printf.sprintf "%s%s%s(%s)"

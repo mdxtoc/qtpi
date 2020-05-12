8bb48ecf2473c44f6f551ca5304e55d934eecb15
Richard Bornat <richard@bornat.me.uk>
2020-05-12 10:33:13 +0100

    fixed free names of recursive pdecl (same as non-recursive, d'oh!)

--- process.ml
+++ process.ml
@@ -341,8 +341,7 @@ let rec frees proc =
 let pdecl_frees (brec,pn,params,proc) =
   let procfrees = frees proc in
   let names = List.map name_of_param params in
-  let r = Name.subtractnames names procfrees in
-  if brec then Name.subtractname (tinst pn) r else r
+  Name.subtractnames names procfrees 
   
 (* fold (left) over a process. optp x p delivers Some x' when it knows, None when it doesn't. *)
 
